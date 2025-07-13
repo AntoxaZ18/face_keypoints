6ac8f01d53f7d9a87b93e0faf5c6f30351a79349
Anton <antoxaz18@gmail.com>
2025-07-13 12:31:06 +0400

    initial

---
+++ datasets.py
@@ -0,0 +1,158 @@
+from torch.utils.data import DataLoader, Dataset
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import matplotlib.pyplot as plt
+import json
+import cv2
+import numpy as np
+import os
+import shutil
+from tqdm import tqdm
+
+class FaceKeyPointDataset(Dataset):
+    def __init__(self, image_path: str, json_path: str, transform = None):
+        super().__init__()
+        self.images = image_path
+        self.annotations = {int(k): v for k, v in json.load(open(json_path, 'r', encoding="utf-8")).items()} 
+        self.transform = transform
+
+    def __getitem__(self, index):
+        sample = self.annotations[index]
+        
+        landmarks = np.array(sample['face_landmarks'], dtype=np.float32) 
+
+        image = cv2.imread(f"{self.images}/{sample['file_name']}")
+        image = cv2.resize(image, (256, 256)) 
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        image = np.array(image)
+
+        landmarks[:, :] = np.clip(landmarks[:, :], 0, 511.5)
+        landmarks = np.array([x / 2 for x in landmarks[::2]] + [y / 2 for y in landmarks[1::2]])
+        image, landmarks = self.apply_transform(image, landmarks)
+
+        landmarks = np.array([x / 256 for x in landmarks[::2]] + [y / 256 for y in landmarks[1::2]])
+
+
+        return image, landmarks
+    
+    def __len__(self):
+        return len(self.annotations)
+    
+    def apply_transform(self, img, face_landmarks):
+        # Применяем трансформацию к изображению
+        if self.transform:
+            transformed = self.transform(image=img, keypoints=face_landmarks)
+            
+            return transformed['image'], transformed['keypoints']
+        else:
+            return img, face_landmarks
+
+# Трансформации для train
+train_transform = A.Compose([
+    A.Resize(256, 256),
+    A.HorizontalFlip(p=0.5),
+    A.RandomBrightnessContrast(p=0.3),
+    # A.Rotate(limit=10, p=0.3),
+    # A.Affine(
+    #     translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)},  # Сдвиг
+    #     scale=(0.8, 1.2),  # Масштабирование
+    #     rotate=(-10, 10),  # Поворот
+    #     p=0.3
+    # ),
+    A.GaussianBlur(blur_limit=(3, 7), p=0.1),
+    A.HueSaturationValue(p=0.3),
+    A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ToTensorV2()
+], keypoint_params=A.KeypointParams(format='xy'))
+
+# Трансформации для val
+val_transform = A.Compose([
+    A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ToTensorV2()
+], keypoint_params=A.KeypointParams(format='xy'))
+
+
+def show_image_with_landmarks(image, landmarks, title="Image with Landmarks"):
+    plt.figure(figsize=(10, 10))
+    if image.shape[0] == 3:
+        image = np.transpose(image, (1, 2, 0))  # Теперь форма (h, w, 3)
+    plt.imshow(image)
+    
+    # Наносим точки
+    for x, y in landmarks:
+        plt.plot(x, y, 'r.', markersize=10)
+    
+    plt.title("Image with Landmarks")
+    plt.axis('off')
+    plt.savefig("output.png")  # Сохраняет изображение в файл
+    plt.close()
+
+
+def create_dataset(images_src, images_dst, annotations, indexes):
+    new_annotations = {k: v for k, v in annotations.items() if k in indexes} #filter test annotation
+
+    os.makedirs(f"{images_dst}/images", exist_ok=True)
+
+    print(f"create dataset in {images_dst}")
+
+    bar = tqdm(indexes)
+    for key in bar:
+        shutil.copy(f"{images_src}/images/{annotations[key]['file_name']}", f"{images_dst}/images/{annotations[key]['file_name']}")
+    
+    new_annotations = {k: v for k, v in zip(range(len(new_annotations)), new_annotations.values())}  #recreate index
+
+    print(f"json saved in {images_dst}/data.json")
+
+    json.dump(new_annotations, open(f"{images_dst}/data.json", "w+"))
+
+
+
+
+
+def train_test_split(original_path, train_path, test_path, original_json:str, test_size):
+    import random 
+    random.seed(42)
+    annotations = {int(k): v for k, v in json.load(open(original_json, 'r', encoding="utf-8")).items()} 
+
+    test_keys = set(random.sample(list(annotations.keys()), int(test_size * len(annotations))))
+    train_keys = set(k for k in annotations.keys() if k not in test_keys)
+
+
+    create_dataset(original_path, train_path, annotations, train_keys)
+    create_dataset(original_path, test_path, annotations, test_keys)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    dataset = FaceKeyPointDataset("./train/images", "./train/data.json")
+
+
+    image, points = dataset[0]
+
+    print(points)
+
+    # # for i in dataset:
+    # #     assert i[1].shape == (68, 2)
+
+    # data_loader = DataLoader(dataset, batch_size=16, num_workers=0)
+
+
+    # for idx, (images, landmarks) in enumerate(data_loader):
+    #     print(idx, landmarks.shape)
+
+    # image, keypoints = dataset[3305]
+
+    # print(keypoints)
+
+    # show_image_with_landmarks(image, keypoints)
+
+
+    # train_test_split("original", "./train", "./test", "./original/all_data.json", 0.2)
---
+++ export_onnx.py
@@ -0,0 +1,85 @@
+import argparse
+
+import torch
+import torch.nn as nn
+from onnxoptimizer import optimize
+from onnxsim import simplify
+
+import onnx
+import torchvision.models as models
+from torchvision.models import MobileNet_V3_Large_Weights
+
+
+
+class LandmarkModel(nn.Module):
+    def __init__(self, base_model, num_features, num_landmarks):
+        super().__init__()
+        self.base = nn.Sequential(*list(base_model.children())[:-1])  # Все кроме classifier
+        self.regressor = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(num_features, 512),
+            nn.ReLU(),
+            nn.BatchNorm1d(512),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_landmarks * 2)
+        )
+
+    def forward(self, x):
+        x = self.base(x)
+        x = x.view(x.size(0), -1)  
+        x = self.regressor(x)
+        return x
+
+
+def export(load_checkpoint: str, output_name: str) -> None:
+
+    base_model = models.mobilenet_v3_large(weights=MobileNet_V3_Large_Weights.DEFAULT)
+    num_ftrs = base_model.classifier[0].in_features
+
+    model = LandmarkModel(base_model, num_ftrs, 68)
+
+
+    model.load_state_dict(torch.load(load_checkpoint))
+
+    model.eval()
+
+    example_inputs = torch.randn(1, 3, 256, 256)
+    # Экспорт в ONNX
+    torch.onnx.export(
+        model,  # модель
+        example_inputs,  # пример входа
+        output_name,  # путь к файлу
+        export_params=True,  # сохранить веса в модели
+        input_names=["input"],  # имена входов
+        output_names=["output"],  # имена выходов
+        opset_version=20,  # for affine transform
+        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+    )
+
+
+def optimize_onnx(model_path: str, optimized_model_path: str):
+    model = onnx.load(model_path)
+
+    # Оптимизация
+    optimized_model = optimize(model)
+
+    # Упрощение
+    optimized_model, _ = simplify(optimized_model)
+
+    onnx.save(model, optimized_model_path)
+
+
+def validate(model_path: str):
+    try:
+        model = onnx.load(model_path)
+        onnx.checker.check_model(model)
+        print("exported model is ok")
+    except Exception as e:
+        print("exported model error:", e)
+
+
+if __name__ == "__main__":
+
+    export("best_model.pth", "mobilenet_relu.onnx")
+    optimize_onnx("mobilenet_relu.onnx", "mobilenet_relu.onnx")
+    validate("mobilenet_relu.onnx")
